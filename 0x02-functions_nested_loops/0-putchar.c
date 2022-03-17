4ec265f4269bf2bce1f568e740a0cb4bb7388f53
odoublea <oladegakenny@gmail.com>
2022-03-16 17:06:11 -0700

    just print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+/*This program prints and returns 0*/
+#include <stdio.h>
+
+main ()
+{
+	print("_putchar\n");
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions to the ALX C - Functions, nested loops tasks
