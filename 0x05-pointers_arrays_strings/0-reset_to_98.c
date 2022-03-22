d1a972e07714807dff1b885a8758fe6b25e7fa94
odoublea <oladegakenny@gmail.com>
2022-03-21 20:46:35 -0700

    reset pointers to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *
+ * reset_to_98 - takes pointer and updates it value to 98
+ * @n - pointer
+ * return 0
+ *
+ */
+
+void reset_to_98(int *n){
+	*n = 98;
+
+	return(0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains the solutions to theALX Holberton's 0x05 - pointers, arrays and strings tasks 
