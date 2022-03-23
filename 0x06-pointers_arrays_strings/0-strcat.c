2729de48fcae8fe9f232c79f42bcc138f3d1fd8a
unblac <55189801+unblac@users.noreply.github.com>
2022-03-23 18:57:08 +0100

    String concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * 0-strcat.c - a function that concatenates two strings
+ * @dest - destination string
+ * @src - source string
+ * Return - src is appended and pointer to end of dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i=0; *(dest + i) != '\0'; i++)
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x06. C - More pointers, arrays and strings
+
+This directory is about pointers
