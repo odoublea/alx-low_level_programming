5999f51777ad738fbbd4830fe4b54bfd2eeedccf
unblac <55189801+unblac@users.noreply.github.com>
2022-03-23 20:31:26 +0100

    strings copier

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,6 +4,7 @@
  * _strncpy - used to copy strings
  * dest: destination of copied strings
  * src: source of copied strings
+ * n: size of string in bytes from src
  * return: copy and return to destination
  */
 
@@ -12,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = '\0';
+		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
 
