e47fc62e379df79e47a0ca4fbce1acf7f1a7838d
unblac <55189801+unblac@users.noreply.github.com>
2022-03-31 02:21:00 +0100

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,23 +8,23 @@
 
 int _sqrt_recursion(int n)
 {
-return (sqrt(n, 1));
+return (square(n, 1));
 }
 
 /**
- * sqrt - find square root
+ * square - find square root
  * @n: int to find square root
  * @val: square root
  * Return: int
  */
 
-int sqrt(int n, int val)
+int square(int n, int val)
 {
 
 if (val * val == n)
 	return (val);
 else if (val * val < n)
-	return  (sqrt(n, val + 1));
+	return  (square(n, val + 1));
 else
 	return (-1);
 
