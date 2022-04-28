5c63800fe3e8d744d89214c87aeaa83e07fec4d1
unblac <55189801+unblac@users.noreply.github.com>
2022-04-28 23:27:43 +0100

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_to_uint - converts a binary number to an
- * unsigned int.
+ * ui: unsigned int.
  * @b: binary.
  *
  * Return: unsigned int.
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len = 0; b[len] != '\0'; len++);
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2);
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
