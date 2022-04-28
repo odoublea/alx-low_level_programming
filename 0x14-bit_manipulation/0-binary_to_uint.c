c0c57192161163f7e0e71315f47fb7d7fb242b2f
unblac <55189801+unblac@users.noreply.github.com>
2022-04-28 23:26:30 +0100

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,10 +17,9 @@ unsigned int binary_to_uint(const char *b)
 
 	ui = 0;
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
+	for (len = 0; b[len] != '\0'; len++);
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2);
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
