588bede701732e26b508ffd6a6031b8fa481fa95
unblac <55189801+unblac@users.noreply.github.com>
2022-05-02 22:43:26 +0100

    elf header

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,36 @@
 #include "main.h"
-/**
- * read_textfile - reads text file and prints it to stdout
- * @filename: Name of file to be read
- * @letters: exact number of letter to be printed
- * Return: 0 if it fails, else the number of characters
- */
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* Declare variables to be used */
 	int fd;
-	ssize_t numread, numwrit;
+	ssize_t numrd, numwr;
 	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	/* open file */
+
 	fd = open(filename, O_RDONLY);
-	/* check if file opens */
+
 	if (fd == -1)
 		return (0);
-	/* malloc space */
-	buf = malloc(letters);
-	if (buf == NULL)
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	/* Now proceed to read the text file */
-	numread = read(fd, buf, letters);
-	/* write to stdout */
-	numwrit = write(STDOUT_FILENO, buf, numread);
-	free(buf);
+
+	numrd = read(fd, buf, letters);
+	numwr = write(STDOUT_FILENO, buf, numrd);
+
 	close(fd);
-	return (numwrit);
+
+	free(buf);
+
+	return (numwr);
 }
