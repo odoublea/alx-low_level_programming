4365518663842da41593b5d305bc6ef2131d7ccf
odoublea <oladegakenny@gmail.com>
2022-08-17 19:05:46 -0700

    Technical interview preparation pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,17 +4,22 @@
 
 """
 
+
 def island_perimeter(grid):
     """Island Perimeter """
     perimeter = 0
-    r,c = len(grid), len(grid[0])
+    r, c = len(grid), len(grid[0])
     for i in range(r):
         for j in range(c):
-            if grid[i][j] == 0: continue
+            if grid[i][j] == 0:
+                continue
             perimeter += 4
-            if i > 0: perimeter -= grid[i-1][j]
-            if j > 0: perimeter -= grid[i][j-1]
-            if i < r-1: perimeter -= grid[i+1][j]
-            if j < c-1: perimeter -= grid[i][j+1]
+            if i > 0:
+                perimeter -= grid[i-1][j]
+            if j > 0:
+                perimeter -= grid[i][j-1]
+            if i < r-1:
+                perimeter -= grid[i+1][j]
+            if j < c-1:
+                perimeter -= grid[i][j+1]
     return perimeter
-
