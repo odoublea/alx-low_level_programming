27cf1eb1cce270ecc174c268dfb94590d0cc6e33
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-16 11:54:38 -0800

    gigahack 4.0

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/odoublea/alx-low_level_programming/blob/main/0x18-dynamic_libraries/Gigahack.so
-export LD_PRELOAD=/tmp/Gigahack.so
+wget -P /tmp https://github.com/Lexxyla/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
