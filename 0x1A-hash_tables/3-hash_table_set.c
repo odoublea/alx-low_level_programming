43b5d9ce489ae13afa1c32a8461cf6857db03e87
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-02-04 11:41:30 +0000

    betty style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,35 +10,31 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        hash_node_t *node, *new_node;
-        unsigned long int index;
+	hash_node_t *node, *new_node;
+	unsigned long int index;
 
-        if (!ht || !key || !*key || !value)
-                return (0);
-
-        index = key_index((const unsigned char *)key, ht->size);
-        node = ht->array[index];
-
-        while (node)
-        {
-                if (strcmp(key, node->key) == 0)
-                {
-                        free(node->value);
-                        node->value = strdup(value);
-                        return (1);
-                }
-                node = node->next;
-        }
-
-        new_node = malloc(sizeof(hash_node_t));
-        if (!new_node)
-                return (0);
-
-        new_node->key = strdup(key);
-        new_node->value = strdup(value);
-        new_node->next = ht->array[index];
-        ht->array[index] = new_node;
-        return (1);
+	if (!ht || !key || !*key || !value)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(key, node->key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
 
 /**
@@ -51,15 +47,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 hash_node_t *create_new_node(const char *key, const char *value)
 {
-        hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
-        if (!new_node)
-                return (NULL);
-
-        new_node->key = strdup(key);
-        new_node->value = strdup(value);
-        new_node->next = NULL;
-
-        return (new_node);
+	if (!new_node)
+		return (NULL);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+	return (new_node);
 }
-
