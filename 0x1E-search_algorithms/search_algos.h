ad0500a053f1b72385c8f70169e2a524005c9640
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-05-09 23:13:16 +0000

    Binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,7 @@
 #include <stddef.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int recursive_search(int *array, size_t size, int value);
 
 #endif
