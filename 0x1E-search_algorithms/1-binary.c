89bfbb1153fb12013d5936539e27544863eda1c0
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-13 06:16:27 +0100

    jump search now working as expected but still printing twice

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -19,6 +19,8 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t l = 0;
 	size_t r = size - 1;
+	size_t i;
+	size_t mid;
 
 	if (array == NULL)
 		return (-1);
@@ -27,33 +29,29 @@ int binary_search(int *array, size_t size, int value)
 
 	while (l <= r)
 	{
-		size_t i;
-		size_t mid = l + (r - l) / 2;
-
 		printf("Searching in array: ");
-		for (i = l; i < r; ++i)
+		for (i = l; i < r; i++)
 		{
 			printf("%d, ", array[i]);
 		}
 		printf("%d\n", array[i]);
 
+		mid = l + (r - l) / 2;
 
-		if (array[mid] == value)
+		if (array[mid] < value)
+		{
+			l = mid + 1;
+		} else if (array[mid] > value)
+		{
+			r = mid - 1;
+		}
+		else
 		{
 			/* Return the index if the value is found */
 			printf("Found %d at index: %lu\n", value, (unsigned long)mid);
 			return (mid);
 		}
-
-		if (array[mid] < value)
-		{
-			l = mid + 1;
-		} else
-		{
-			r = mid - 1;
-		}
 	}
 
 	return (-1);
-	/* Return -1 if the value is not present in the array */
 }
--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,16 +1,49 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * binary_search - calls to binary_search to return
+ * jump_search - calls to jump_search to return
  * the index of the number
- *
- * This function takes a sorted array of integers,
- * its size, and a value to search for. It returns
- * the index of the value if found, or -1 if the value
- * is not present in the array or if the array is NULL.
  *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
  * Return: index of the number
  */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t jump = sqrt(size);
+	size_t start = 0;
+	size_t end;
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (start < size && array[start] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
+		start += jump;
+		end = (start < size) ? start : size - 1;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			start - jump, end);
+
+	for (i = start - jump; i <= end; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %lu\n", value, i);
+			return (i);
+		}
+	}
+
+	printf("Value %d not found in the array.\n", value);
+	return (-1);
+}
