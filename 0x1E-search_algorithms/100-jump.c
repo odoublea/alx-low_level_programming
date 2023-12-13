e4e7655a57c4efc38d2ca7dc5cfd8472aebd963a
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-13 07:05:04 +0100

    documentation v2

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -7,7 +7,7 @@
  * @array: input array
  * @size: size of the array
  * @value: value to search in
- * Return: index of the number
+ * Return: The first index where value is located, or -1 if not found
  */
 int jump_search(int *array, size_t size, int value)
 {
