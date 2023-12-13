1179ae33820ad5b51c62712cb421b57e99571382
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-13 21:14:08 +0100

    Jump do search in a singly linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -5,7 +5,7 @@
  * @list: Pointer to head of list to search
  * @size: Nodes in list
  * @value: Value to search for
- *
+ * 
  * Return: Pointer to first node where value is located
  * NULL if value not found in head or head is NULL
  */
@@ -15,34 +15,39 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	int jump, i;
 	listint_t *cur, *tmp;
 
-	if (list == NULL)
+	if (!list)
 		return (NULL);
 
 	jump = sqrt(size);
 	cur = list;
 
-	while (cur->next && cur->n < value)
-	{
+	do {
 		tmp = cur;
-
 		for (i = jump; cur->next && i > 0; --i, cur = cur->next)
-			printf("Value checked at index [%lu] = [%d]\n",
-					cur->index, cur->n);
-	}
+			printf("Value checked at index [%lu] = [%d]\n", cur->index, cur->n);
 
-	printf("Value found between indexes [%lu] and [%lu]\n",
-			tmp->index, cur->index);
+		if (!cur->next)
+			break;
+
+	} while (cur->n < value);
+
+	printf("Value found between indexes [%lu] and [%lu]\n", tmp->index, cur->index);
 	cur = tmp;
 
-	while (cur->n < value && cur->next)
+	while (cur->n < value)
 	{
-		printf("Value checked at index [%lu] = [%d]\n",
-				cur->index, cur->n);
+		printf("Value checked at index [%lu] = [%d]\n", cur->index, cur->n);
 		cur = cur->next;
+
+		if (cur->n == value)
+			return (printf("Value checked at index [%lu] = [%d]\n", cur->index, cur->n), cur);
+
+		if (!cur->next)
+			return (printf("Value checked at index [%lu] = [%d]\n", cur->index, cur->n), NULL);
 	}
 
 	if (cur->n == value)
-		return printf("Value checked at index [%lu] = [%d]\n",
-				cur->index, cur->n), cur;
+		return (printf("Value checked at index [%lu] = [%d]\n", cur->index, cur->n), cur);
+
 	return (NULL);
 }
