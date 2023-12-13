6e5e9d3db1782b999b9ae89010f379692f6e7f3b
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-13 21:00:46 +0100

    Jump search in a singly linked list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Search for value in sorted list using Jump search algorithm
+ * @list: Pointer to head of list to search
+ * @size: Nodes in list
+ * @value: Value to search for
+ *
+ * Return: Pointer to first node where value is located
+ * NULL if value not found in head or head is NULL
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	int jump, i;
+	listint_t *cur, *tmp;
+
+	if (list == NULL)
+		return (NULL);
+
+	jump = sqrt(size);
+	cur = list;
+
+	while (cur->next && cur->n < value)
+	{
+		tmp = cur;
+
+		for (i = jump; cur->next && i > 0; --i, cur = cur->next)
+			printf("Value checked at index [%lu] = [%d]\n",
+					cur->index, cur->n);
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			tmp->index, cur->index);
+	cur = tmp;
+
+	while (cur->n < value && cur->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+				cur->index, cur->n);
+		cur = cur->next;
+	}
+
+	if (cur->n == value)
+		return printf("Value checked at index [%lu] = [%d]\n",
+				cur->index, cur->n), cur;
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,4 +14,23 @@ int exponential_search(int *array, size_t size, int value);
 int binary_search_exp(int *array, size_t start, size_t bound, int value);
 int advanced_binary(int *array, size_t size, int value);
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    int value;
+    struct listint_s *next;
+} listint_t;
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
 #endif
