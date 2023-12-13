ef7b7313322c8ca75381adc0d11aec847ad2a348
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-13 06:25:07 +0100

    syntax fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -49,7 +49,6 @@ int binary_search(int *array, size_t size, int value)
 		{
 			/* Return the index if the value is found */
 			return (mid);
-			printf("Found %d at index: %lu\n", value, (unsigned long)mid);
 		}
 	}
 
--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -40,7 +40,6 @@ int jump_search(int *array, size_t size, int value)
 		if (array[i] == value)
 		{
 			return (i);
-			printf("Found %d at index: %lu\n", value, i);
 		}
 	}
 
