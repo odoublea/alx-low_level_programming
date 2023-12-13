1f4350c5188eb58ab5286a568e0984eb66c21683
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-13 12:38:32 +0100

    linear search with while loop

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,17 +12,18 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	while (i < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
-			return ((int)i);
+			return (i);
+		i++;
 	}
 	return (-1);
 }
